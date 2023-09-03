793171f7e1d4618dea23bf9f6ce2b39fa632acb4
iluvshi <shian.morrison@gmail.com>
2023-09-02 20:40:04 -0400

    initial commit, only navbar created

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bckround: "#1d3557",
+        primary: "#a8dadc",
+        secondary: "#457b9d",
+      }
+    },
+  },
+  plugins: [],
+}
+
