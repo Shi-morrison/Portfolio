d7ad5011aadba90b1d0df62ec09cb8df0f7526f3
iluvshi <shian.morrison@gmail.com>
2023-09-04 21:22:35 -0400

    Test backround vid

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
+import Home from './components/Home.vue';
 
 </script>
 
 <template>
   <div>
     <Navbar></Navbar>
+    <Home></Home>
 
 
   </div>
